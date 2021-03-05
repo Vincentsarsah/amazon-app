24e8bb90ef4ac5e95de06147d5f71881c608eca1
Vincentsarsah <vincent.sarsah@amalitech.org>
2021-03-05 12:10:00 -0800

    share code on github

---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products :[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'heigh quality prodcut'
+        },
+        {
+            _id: '2',
+            name: 'Addidas Slim Shirt',
+            category: 'shirt',
+            image: './images/p1.jpg',
+            price: 100,
+            brand: 'Addidas',
+            rating: 4,
+            numReviews: 10,
+            description: 'heigh quality prodcut'
+        },
+        {
+            _id: '3',
+            name: 'Lacosta Slim Shirt',
+            category: 'shirt',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Lacosta',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'heigh quality prodcut'
+        },
+        {
+            _id: '4 ',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: './images/p4.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'heigh quality prodcut'
+        },
+        {
+            _id: '5',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: './images/p5.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'heigh quality prodcut'
+        },
+        {
+            _id: '6',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: './images/p6.jpg',
+             price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'heigh quality prodcut'
+        }
+
+    ]
+}
+
+export default data;
